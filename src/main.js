bdf462fc73d9e304d95ff432740798a914cdf71f
Sabrina <sabrinaacm@gmail.com>
2018-06-13 12:52:00 -0500

    simplificando el llamado de los datos

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-var myRequest = new XMLHttpRequest();
+/*let myRequest = new XMLHttpRequest();
 myRequest.open('GET', '../data/cohorts/lim-2018-03-pre-core-pw/users.json');
 myRequest.onload = function () {
     var myData = JSON.parse(myRequest.responseText);
     console.log(myData);
 };
 myRequest.send();
+*/
+let myRequest1 = new XMLHttpRequest();
+myRequest1.open('GET', '../data/cohorts/lim-2018-03-pre-core-pw/users.json');
+let users = JSON.parse(myRequest1.responseText);
+console.log(users);
