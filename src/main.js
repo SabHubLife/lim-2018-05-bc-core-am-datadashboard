de872a3af3a1fc73b27c11d29e3ac23b42864139
Sab DevSet <sabrinaacm@gmail.com>
2018-06-14 06:01:19 -0500

    conectando la data 2

--- src/main.js
+++ src/main.js
@@ -1,12 +1,35 @@
-/*let myRequest = new XMLHttpRequest();
-myRequest.open('GET', '../data/cohorts/lim-2018-03-pre-core-pw/users.json');
-myRequest.onload = function () {
-    var myData = JSON.parse(myRequest.responseText);
-    console.log(myData);
-};
-myRequest.send();
-*/
 let myRequest1 = new XMLHttpRequest();
 myRequest1.open('GET', '../data/cohorts/lim-2018-03-pre-core-pw/users.json');
-let users = JSON.parse(myRequest1.responseText);
-console.log(users);
+myRequest1.onload = function() {
+    let users = JSON.parse(myRequest1.responseText);
+    //document.getElementById('user').innerHTML = JSON.stringify(users);
+    console.log(users);
+};
+
+let myRequest2 = new XMLHttpRequest();
+myRequest2.open('GET', '../data/cohorts/lim-2018-03-pre-core-pw/progress.json');
+myRequest2.onload = function() {
+    let progress = JSON.parse(myRequest2.responseText);
+    //document.getElementById('user').innerHTML = JSON.stringify(progress);
+    console.log(progress);
+};
+
+let myRequest3 = new XMLHttpRequest();
+myRequest3.open('GET', '../data/cohorts.json');
+myRequest3.onload = function() {
+    let cohorts = JSON.parse(myRequest3.responseText);
+    //document.getElementById('user').innerHTML = JSON.stringify(cohorts);
+    console.log(cohorts);
+};
+
+myRequest1.send();
+myRequest2.send();
+myRequest3.send();
+
+let users = myRequest1;
+let progress = myRequest2;
+let cohorts = myRequest3;
+
+window.onload = () => {
+    document.getElementById("showusers").innerHTML = `${myRequest1.onload}`
+};
