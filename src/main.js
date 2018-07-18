b7c7954b1be36170147697c07faaa064d97b7ef2
Sab DevSet <sabrinaacm@gmail.com>
2018-07-18 04:50:14 -0500

    select box

--- src/main.js
+++ src/main.js
@@ -29,36 +29,18 @@ const showUsers = (user, dataUsers) => {
 }
 
 const showCohorts = (city, dataCohorts) => {
-    mainSection.innerHTML = `<select class="form-control" id='select'></select>`
+    mainSection.innerHTML = `<select style ="border-color: black; box-shadow: 0.2em 0.2em black;" class="form-control" id='select'></select>`
     const cohortByCity = dataCohorts.filter(cohort => {
         return cohort.id.indexOf(city) !== -1;
     })
     cohortByCity.forEach(cohort => {
         const options = document.getElementById('select')
         options.innerHTML +=
-<<<<<<< HEAD
-            `<option id='${cohort.id}'>${cohort.id}</option>`
-=======
-            `<option id"${cohort.id}"=>${cohort.id}</option>`
->>>>>>> 3f45fc7e6a2dfd0b8d8bbd0e041bed4d95627e8b
+            `<option style ="background: rgb(255, 229, 33);" id"${cohort.id}"=>${cohort.id}</option>`
     });
 };
-
-const showProgress = (cohortName,dataProgress)=>{
-
-};
-
-const showUsers = (cohortName, dataUsers)=>{
-
-};
-
 
 listVenues.addEventListener('click', event => {
     mainSection.innerHTML = '';
     getData(event.target.id, '../data/cohorts.json', showCohorts);
-});
-
-mainSection.addEventListener('change', event =>{
-    debugger
-    getData(event.target.id, '../data/cohorts/lim-2018-03-pre-core-pw/users.json', showUsers);
 });
